b813efc31c8383fedcda7185a3dbbd1da17cca89
Alexandros Trifyllis <alexandros.trifyllis@trasysgroup.com>
2017-02-02 12:08:08 +0200

    before refactoring selectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { UserSelectionComponent } from './user-selection/user-selection.component';
-import { ThreadSectionComponent } from './thread-section/thread-section.component';
-import { MessageSectionComponent } from './message-section/message-section.component';
-import { ThreadListComponent } from './thread-list/thread-list.component';
-import { MessageListComponent } from './message-list/message-list.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {UserSelectionComponent} from "./user-selection/user-selection.component";
+import {ThreadSectionComponent} from "./thread-section/thread-section.component";
+import {MessageSectionComponent} from "./message-section/message-section.component";
+import {ThreadListComponent} from "./thread-list/thread-list.component";
+import {MessageListComponent} from "./message-list/message-list.component";
 import {ThreadsService} from "./services/threads.service";
+import {StoreModule, Action} from "@ngrx/store";
+import {INITIAL_APPLICATION_STATE, ApplicationState} from "./store/application-state";
+import {LOAD_USER_THREAD_ACTION, LoadUserThreadsAction} from "./store/actions";
+import * as _ from 'lodash';
+
+function storeReducer(state: ApplicationState = INITIAL_APPLICATION_STATE,
+                      action: Action): ApplicationState {
+  switch (action.type) {
+    case LOAD_USER_THREAD_ACTION:
+      return handleLoadUserThreadsAction(state, action);
+
+    default:
+      return state;
+  }
+
+
+}
+
+function handleLoadUserThreadsAction(state: ApplicationState,
+                                     action: LoadUserThreadsAction): ApplicationState {
+  const userData = action.payload;
+  const newState:ApplicationState = Object.assign({}, state)
+
+  newState.storeData = {
+    participants: _.keyBy(action.payload.participants, 'id'),
+    messages: _.keyBy(action.payload.messages, 'id'),
+    threads: _.keyBy(action.payload.threads, 'id'),
+  }
+  return newState;
+}
 
 @NgModule({
   declarations: [
@@ -23,9 +52,11 @@ import {ThreadsService} from "./services/threads.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore(storeReducer)
   ],
   providers: [ThreadsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {ThreadsService} from "../services/threads.service";
-
+import {Store} from "@ngrx/store";
+import {ApplicationState} from "../store/application-state";
+import {LoadUserThreadsAction} from "../store/actions";
+import {Observable} from "rxjs";
+import {Thread} from "../../../shared/model/thread";
+import * as _ from "lodash";
+import {ThreadSummaryVM} from "./thread-summary.vm";
+import {mapStateToUserName} from "./mapStateToUserName";
+import {mapStateToUnreadMessagesCounter} from "./mapStateToUnreadMessagesCounter";
+import {stateToThreadSummariesSelector} from "./stateToThreadSummariesSelector";
 
 @Component({
   selector: 'thread-section',
@@ -9,15 +18,33 @@ import {ThreadsService} from "../services/threads.service";
 })
 export class ThreadSectionComponent implements OnInit {
 
-  constructor(private threadsService: ThreadsService) {
+  userName$: Observable<string>;
+  unreadMessagesCounter$: Observable<number>;
+  threadSummaries$: Observable<ThreadSummaryVM[]>;
+
+  constructor(private threadsService: ThreadsService,
+              private store: Store<ApplicationState>) {
+
+    this.userName$ = store
+      .skip(1)
+      .map(mapStateToUserName)
+    ;
+
+    this.unreadMessagesCounter$ = store
+      .skip(1)
+      .map(mapStateToUnreadMessagesCounter)
+    ;
+
+    this.threadSummaries$ = store.select(stateToThreadSummariesSelector);
 
   }
 
-
-
   ngOnInit() {
 
-        this.threadsService.loadUserThreads();
+    this.threadsService.loadUserThreads()
+      .subscribe(
+        allUserData => this.store.dispatch(new LoadUserThreadsAction(allUserData))
+      );
 
   }
 
